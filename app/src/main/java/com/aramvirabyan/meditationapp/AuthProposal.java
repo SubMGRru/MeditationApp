484144b4a1a5564b41cd0bbf2109f226f5089b1d
CleanThat <CleanThat>
2022-05-25 16:37:00 +0000

    language=java nb_files_formatted: 2

--- app/src/main/java/com/aramvirabyan/meditationapp/AuthProposal.java
+++ app/src/main/java/com/aramvirabyan/meditationapp/AuthProposal.java
@@ -20,64 +20,67 @@ import java.io.InputStream;
 
 public class AuthProposal extends AppCompatActivity {
 
-    ImageView authproposal_image;
-    TextView authproposal_title;
-    TextView authproposal_subtitle;
+  ImageView authproposal_image;
+  TextView authproposal_title;
+  TextView authproposal_subtitle;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth_proposal);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_auth_proposal);
 
-        authproposal_title = findViewById(R.id.authproposal_title);
-        authproposal_subtitle = findViewById(R.id.authproposal_subtitle);
+    authproposal_title = findViewById(R.id.authproposal_title);
+    authproposal_subtitle = findViewById(R.id.authproposal_subtitle);
 
-        authproposal_image = findViewById(R.id.authproposal_image);
-        String filename = "Images/Icons/3d-fluency-globe.webp";
-        try(InputStream inputStream = getApplicationContext().getAssets().open(filename)) {
-            Drawable drawable = Drawable.createFromStream(inputStream, null);
-            authproposal_image.setImageDrawable(drawable);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ObjectAnimator moveUpY = ObjectAnimator.ofFloat(authproposal_image, "translationY", -529);
-        moveUpY.setDuration(800);
-        AnimatorSet moveUp = new AnimatorSet();
-        moveUp.play(moveUpY);
-        moveUp.start();
-
-        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(authproposal_image, "scaleX", 2.0f);
-        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(authproposal_image, "scaleY", 2.0f);
-        scaleDownX.setDuration(1000);
-        scaleDownY.setDuration(1000);
-
-        AnimatorSet scaleDown = new AnimatorSet();
-
-        scaleDown.play(scaleDownX).with (scaleDownY) 
-        ;
-
-        scaleDown.start();
-
-        authproposal_title.setVisibility(View.GONE);
-        authproposal_subtitle.setVisibility(View.GONE);
-
-        TranslateAnimation animate_forTitle = new TranslateAnimation(0, authproposal_title.getWidth(), 0, 0);
-        animate_forTitle.setDuration(3500);
-        animate_forTitle.setFillAfter(true);
-        authproposal_title.startAnimation(animate_forTitle);
-        authproposal_title.setVisibility(View.VISIBLE);
-
-        TranslateAnimation animate_forSubtitle = new TranslateAnimation(0, authproposal_subtitle.getWidth(), 0, 0);
-        animate_forSubtitle.setDuration(3500);
-        animate_forSubtitle.setFillAfter(true);
-        authproposal_subtitle.startAnimation(animate_forSubtitle);
-        authproposal_subtitle.setVisibility(View.VISIBLE);
+    authproposal_image = findViewById(R.id.authproposal_image);
+    String filename = "Images/Icons/3d-fluency-globe.webp";
+    try (InputStream inputStream = getApplicationContext().getAssets().open(filename)) {
+      Drawable drawable = Drawable.createFromStream(inputStream, null);
+      authproposal_image.setImageDrawable(drawable);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    public void call_mainAuth(android.view.View viewBox) {
-        AuthService.mainAuth(this);
-    }
-    public void call_anonAuth(android.view.View viewBox) {
-        AuthService.anonymousAuth(this, viewBox);
-    }
+    ObjectAnimator moveUpY = ObjectAnimator.ofFloat(authproposal_image, "translationY", -529);
+    moveUpY.setDuration(800);
+    AnimatorSet moveUp = new AnimatorSet();
+    moveUp.play(moveUpY);
+    moveUp.start();
+
+    ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(authproposal_image, "scaleX", 2.0f);
+    ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(authproposal_image, "scaleY", 2.0f);
+    scaleDownX.setDuration(1000);
+    scaleDownY.setDuration(1000);
+
+    AnimatorSet scaleDown = new AnimatorSet();
+
+    scaleDown.play(scaleDownX).with(scaleDownY);
+
+    scaleDown.start();
+
+    authproposal_title.setVisibility(View.GONE);
+    authproposal_subtitle.setVisibility(View.GONE);
+
+    TranslateAnimation animate_forTitle =
+        new TranslateAnimation(0, authproposal_title.getWidth(), 0, 0);
+    animate_forTitle.setDuration(3500);
+    animate_forTitle.setFillAfter(true);
+    authproposal_title.startAnimation(animate_forTitle);
+    authproposal_title.setVisibility(View.VISIBLE);
+
+    TranslateAnimation animate_forSubtitle =
+        new TranslateAnimation(0, authproposal_subtitle.getWidth(), 0, 0);
+    animate_forSubtitle.setDuration(3500);
+    animate_forSubtitle.setFillAfter(true);
+    authproposal_subtitle.startAnimation(animate_forSubtitle);
+    authproposal_subtitle.setVisibility(View.VISIBLE);
+  }
+
+  public void call_mainAuth(android.view.View viewBox) {
+    AuthService.mainAuth(this);
+  }
+
+  public void call_anonAuth(android.view.View viewBox) {
+    AuthService.anonymousAuth(this, viewBox);
+  }
 }
--- app/src/main/java/com/aramvirabyan/meditationapp/AuthService.java
+++ app/src/main/java/com/aramvirabyan/meditationapp/AuthService.java
@@ -25,62 +25,69 @@ import org.json.JSONObject;
 
 public class AuthService {
 
-    public static String new_auth_token;
+  public static String new_auth_token;
 
-    public static void anonymousAuth(Context context, android.view.View viewBox) {
+  public static void anonymousAuth(Context context, android.view.View viewBox) {
 
-        RequestQueue queue = Volley.newRequestQueue(context);
-        Gson g = new Gson();
+    RequestQueue queue = Volley.newRequestQueue(context);
+    Gson g = new Gson();
 
-        Snackbar snackbar = Snackbar.make(viewBox, context.getString(R.string.snackbar_connectionAwait), Snackbar.LENGTH_INDEFINITE);
-        snackbar.show();
+    Snackbar snackbar = Snackbar.make(viewBox, context.getString(R.string.snackbar_connectionAwait),
+        Snackbar.LENGTH_INDEFINITE);
+    snackbar.show();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, context.getString(R.string.api_server) + "/service/auth_anonymousAccess", new Response.Listener<String>() {
-            @Override public void onResponse(String response) {
-                // Display the first 500 characters of the response string.
+    StringRequest stringRequest = new StringRequest(Request.Method.GET,
+        context.getString(R.string.api_server) + "/service/auth_anonymousAccess",
+        new Response.Listener<String>() {
+          @Override
+          public void onResponse(String response) {
+            // Display the first 500 characters of the response string.
 
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    new_auth_token = jsonObject.getString("auth_token");
-                    //Toast.makeText(context, "Result: " + new_auth_token, Toast.LENGTH_LONG).show();
-                    save_AuthData(context, new_auth_token);
-                    snackbar.dismiss();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
+            try {
+              JSONObject jsonObject = new JSONObject(response);
+              new_auth_token = jsonObject.getString("auth_token");
+              // Toast.makeText(context, "Result: " + new_auth_token, Toast.LENGTH_LONG).show();
+              save_AuthData(context, new_auth_token);
+              snackbar.dismiss();
+            } catch (JSONException e) {
+              e.printStackTrace();
             }
+
+          }
         }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                snackbar.dismiss();
-                if (error instanceof TimeoutError || error instanceof ServerError || error instanceof ParseError || error instanceof NetworkError) {
-                    context.startActivity(new Intent(context, NoConnection.class).putExtra("reasonState", "unavailable_server"));
-                    ((Activity)context).finish();
-                } else if (error instanceof NoConnectionError) {
-                    context.startActivity(new Intent(context, NoConnection.class).putExtra("reasonState", "no_connection"));
-                    ((Activity)context).finish();
-                } else {
-                    Toast.makeText(context, "We have an error: " + error, Toast.LENGTH_LONG).show();
-                }
+          @Override
+          public void onErrorResponse(VolleyError error) {
+            snackbar.dismiss();
+            if (error instanceof TimeoutError || error instanceof ServerError
+                || error instanceof ParseError || error instanceof NetworkError) {
+              context.startActivity(new Intent(context, NoConnection.class).putExtra("reasonState",
+                  "unavailable_server"));
+              ((Activity) context).finish();
+            } else if (error instanceof NoConnectionError) {
+              context.startActivity(
+                  new Intent(context, NoConnection.class).putExtra("reasonState", "no_connection"));
+              ((Activity) context).finish();
+            } else {
+              Toast.makeText(context, "We have an error: " + error, Toast.LENGTH_LONG).show();
             }
+          }
         });
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
+    // Add the request to the RequestQueue.
+    queue.add(stringRequest);
+  }
 
-    public static void mainAuth(Context context) {
-        context.startActivity(new Intent(context, AuthExternal.class));
-    }
+  public static void mainAuth(Context context) {
+    context.startActivity(new Intent(context, AuthExternal.class));
+  }
 
-    public static void save_AuthData(Context context, String token) {
-        SharedPreferences preferences = context.getSharedPreferences("app_data", 0);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("auth_token", token);
-        editor.apply();
+  public static void save_AuthData(Context context, String token) {
+    SharedPreferences preferences = context.getSharedPreferences("app_data", 0);
+    SharedPreferences.Editor editor = preferences.edit();
+    editor.putString("auth_token", token);
+    editor.apply();
 
-        context.startActivity(new Intent(context, PreferencesManager.class));
-        ((Activity)context).finish();
-    }
+    context.startActivity(new Intent(context, PreferencesManager.class));
+    ((Activity) context).finish();
+  }
 }
