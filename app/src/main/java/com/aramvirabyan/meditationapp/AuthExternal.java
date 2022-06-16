4bfc679cda27c0a2810e253e51f978e33bb7afcb
CleanThat <CleanThat>
2022-06-16 00:55:08 +0000

    language=java nb_files_neither_included_nor_excluded: 1 nb_files_formatted: 2

--- app/src/main/java/com/aramvirabyan/meditationapp/AuthExternal.java
+++ app/src/main/java/com/aramvirabyan/meditationapp/AuthExternal.java
@@ -11,21 +11,23 @@ import android.webkit.WebViewClient;
 
 public class AuthExternal extends AppCompatActivity {
 
-    public Context context;
+  public Context context;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth_external);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_auth_external);
 
-        context = getApplicationContext();
+    context = getApplicationContext();
 
-        WebView webView = findViewById(R.id.authexternal_wvbox);
-        webView.setWebViewClient(new WebViewClient());
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setJavaScriptEnabled(true);
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        webView.addJavascriptInterface(new AuthExternalInterface(this), "Android");
-        webView.loadUrl(context.getString(R.string.staticRenderer_server) + "/auth/?source=androidapp&api_server=" + context.getString(R.string.api_server));
-    }
+    WebView webView = findViewById(R.id.authexternal_wvbox);
+    webView.setWebViewClient(new WebViewClient());
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setDomStorageEnabled(true);
+    webSettings.setJavaScriptEnabled(true);
+    webView.setBackgroundColor(Color.TRANSPARENT);
+    webView.addJavascriptInterface(new AuthExternalInterface(this), "Android");
+    webView.loadUrl(context.getString(R.string.staticRenderer_server)
+        + "/auth/?source=androidapp&api_server=" + context.getString(R.string.api_server));
+  }
 }
